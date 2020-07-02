c886e8a2b23f869c488642ffb9caa207ade3a90a
ritik <sinharitik18112835@gmail.com>
2020-07-02 15:13:48 +0530

    form component initailised

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const Form = () => {
+  return (
+    <div className="form_container">
+      <div className="input_label">Company </div>
+      <input className="input_field_container" />
+    </div>
+  );
+};
+
+export default Form;
